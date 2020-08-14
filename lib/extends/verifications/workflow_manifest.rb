8379033acd5b7b09aaa01fa4aec56aec45f4316f
sergei-krylov <sergei.krylov@yahoo.com>
2020-08-14 13:49:16 +0300

    BOSA-77 Fix spec tests - add missing attribute

--- lib/extends/verifications/workflow_manifest.rb
+++ lib/extends/verifications/workflow_manifest.rb
@@ -8,6 +8,7 @@ module WorkflowManifestExtend
 
     attribute :omniauth_provider, String
     attribute :minimum_age, Integer, default: 0
+    attribute :anti_affinity, Array[String], default: []
 
   end
 end
