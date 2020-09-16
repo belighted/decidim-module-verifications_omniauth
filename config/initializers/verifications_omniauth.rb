3ee168de403d4f8dc9ff6d86408f454b1638cd5e
Adam Okoń <adam.okon@elegantcode.pl>
2020-09-16 09:18:37 +0200

    BOSA-97: migrate code from the app scope to extension
    Migrated:
    * omniauth initializer
    * omniauth strategies for saml and csam

--- config/initializers/verifications_omniauth.rb
+++ config/initializers/verifications_omniauth.rb
@@ -1,13 +1,3 @@
 # frozen_string_literal: true
 
 require "extends/verifications/workflow_manifest"
-
-# in the app initializer...
-# Decidim::Verifications.register_workflow(:saml) do |workflow|
-#   workflow.engine = Decidim::Verifications::Omniauth::Engine
-#   workflow.admin_engine = Decidim::Verifications::Omniauth::AdminEngine
-#   workflow.action_authorizer = "Decidim::Verifications::Omniauth::BosaActionAuthorizer"
-#   # workflow.form = "Decidim::Verifications::Omniauth::OmniauthAuthorizationForm"
-#   workflow.omniauth_provider = :saml
-#   workflow.minimum_age = 16
-# end
