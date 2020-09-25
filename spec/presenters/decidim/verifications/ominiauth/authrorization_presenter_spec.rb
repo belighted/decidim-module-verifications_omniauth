ff4a3c9e598d4e6a76d0958e8a8279e3e680b04e
Adam Okoń <adam.okon@elegantcode.pl>
2020-09-25 17:15:45 +0200

    BOSA-99: add specs for authrorization_presenter

---
+++ spec/presenters/decidim/verifications/ominiauth/authrorization_presenter_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Decidim::Verifications::Omniauth::AuthorizationPresenter do
+  let(:presenter) { described_class.new(authorization) }
+
+  describe "#authorized?" do
+    subject { presenter.authorized? }
+
+    context "when is truthly" do
+      let(:authorization) { build_stubbed(:authorization, verification_metadata: { "authorized" => true }) }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context "when is falsy" do
+      let(:authorization) { build_stubbed(:authorization, verification_metadata: { "authorized" => false }) }
+
+      it { is_expected.to be_falsy }
+    end
+  end
+end
