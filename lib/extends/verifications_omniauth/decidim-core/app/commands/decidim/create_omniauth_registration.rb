04571dd918596c9d5d7ad50f783eb4ae2c2b979a
sergei-krylov <sergei.krylov@yahoo.com>
2020-11-19 16:00:18 +0300

    BOSA-178 Fix spec tests in decidim-module-verifications_omniauth - fix create omniauth registration form

--- lib/extends/verifications_omniauth/decidim-core/app/commands/decidim/create_omniauth_registration.rb
+++ lib/extends/verifications_omniauth/decidim-core/app/commands/decidim/create_omniauth_registration.rb
@@ -6,6 +6,13 @@ module CreateOmniauthRegistrationExtend
   extend ActiveSupport::Concern
 
   included do
+
+    def initialize(form, verified_email = nil)
+      @form = form
+      @verified_email = verified_email
+      @after_confirmation = false
+    end
+
     def call
       verify_oauth_signature!
 
@@ -16,9 +23,6 @@ module CreateOmniauthRegistrationExtend
 
           return broadcast(:ok, user)
         end
-
-        return broadcast(:error, user) if request.path.end_with?("saml/callback") || request.path.end_with?("csam/callback")
-
         return broadcast(:invalid) if form.invalid?
 
         transaction do
@@ -33,6 +37,61 @@ module CreateOmniauthRegistrationExtend
         broadcast(:error, e.record)
       end
     end
+
+    private
+
+    # rubocop:disable Metrics/PerceivedComplexity
+    def create_or_find_user
+      generated_password = SecureRandom.hex
+
+      if (verified_email || form.email).blank?
+
+        @user = Decidim::User.new(
+          email: "",
+          organization: organization,
+          name: form.name,
+          nickname: form.nickname,
+          newsletter_notifications_at: nil,
+          email_on_notification: false,
+          accepted_tos_version: organization.tos_version,
+          # managed: true,
+          password: generated_password,
+          password_confirmation: generated_password
+        )
+        @user.skip_confirmation!
+
+      else
+
+        @user = Decidim::User.find_or_initialize_by(
+          email: verified_email,
+          organization: organization
+        )
+
+        if @user.persisted?
+          # If user has left the account unconfirmed and later on decides to sign
+          # in with omniauth with an already verified account, the account needs
+          # to be marked confirmed.
+          @user.skip_confirmation! if !@user.confirmed? && @user.email == verified_email
+        else
+          @user.email = (form.email || verified_email)
+          @user.name = form.name
+          @user.nickname = form.normalized_nickname
+          @user.newsletter_notifications_at = nil
+          @user.email_on_notification = true
+          @user.password = generated_password
+          @user.password_confirmation = generated_password
+          @user.remote_avatar_url = form.avatar_url if form.avatar_url.present?
+          @user.skip_confirmation! unless verified_email != form.email
+          @after_confirmation = (verified_email != form.email)
+        end
+
+      end
+
+      @user.tos_agreement = "1"
+      @user.save!
+    end
+    # rubocop:enable Metrics/PerceivedComplexity
+
   end
 end
 
--- lib/extends/verifications_omniauth/decidim-core/app/forms/decidim/omniauth_registration_form.rb
+++ lib/extends/verifications_omniauth/decidim-core/app/forms/decidim/omniauth_registration_form.rb
@@ -6,11 +6,11 @@ module OmniauthRegistrationFormExtend
   extend ActiveSupport::Concern
 
   included do
-    validate :email, :email_is_unique, unless: -> { email.blank? }
+    #validate :email, :email_is_unique, unless: -> { email.blank? }
 
-    def email_is_unique
-      errors.add(:email, "#{email} #{I18n.t("errors.messages.taken")}") if Decidim::User.where(organization: current_organization, email: email).exists?
-    end
+    # def email_is_unique
+    #   errors.add(:email, "#{email} #{I18n.t("errors.messages.taken")}") if Decidim::User.where(organization: current_organization, email: email).exists?
+    # end
   end
 end
 
