6d11b622fcb1c7c1ba13c591fbb00941dd7c4e9e
sergei-krylov <sergei.krylov@yahoo.com>
2021-05-25 15:49:05 +0300

    BOSA21Q1-315 Use a hash of national registry number to link CSAM connection to the right user

--- lib/extends/verifications_omniauth/decidim-core/app/commands/decidim/create_omniauth_registration.rb
+++ lib/extends/verifications_omniauth/decidim-core/app/commands/decidim/create_omniauth_registration.rb
@@ -62,48 +62,91 @@ module CreateOmniauthRegistrationExtend
     end
 
     # rubocop:disable Metrics/PerceivedComplexity
+    # def create_or_find_user
+    #   generated_password = SecureRandom.hex
+    #
+    #   if verified_email.blank?
+    #     @user = Decidim::User.new(
+    #       email: "",
+    #       organization: organization,
+    #       name: form.name,
+    #       nickname: form.normalized_nickname,
+    #       newsletter_notifications_at: nil,
+    #       email_on_notification: false,
+    #       password: generated_password,
+    #       password_confirmation: generated_password
+    #     )
+    #     @user.skip_confirmation!
+    #   else
+    #     @user = Decidim::User.find_or_initialize_by(
+    #       email: verified_email,
+    #       organization: organization
+    #     )
+    #
+    #     if @user.persisted?
+    #       # If user has left the account unconfirmed and later on decides to sign
+    #       # in with omniauth with an already verified account, the account needs
+    #       # to be marked confirmed.
+    #       @user.skip_confirmation! if !@user.confirmed? && @user.email == verified_email
+    #     else
+    #       @user.name = form.name
+    #       @user.email = form.email || verified_email
+    #       @user.nickname = form.normalized_nickname
+    #       @user.newsletter_notifications_at = nil
+    #       @user.email_on_notification = true
+    #       @user.password = generated_password
+    #       @user.password_confirmation = generated_password
+    #       @user.remote_avatar_url = form.avatar_url if form.avatar_url.present?
+    #       @user.skip_confirmation!
+    #     end
+    #   end
+    #
+    #   @user.save!
+    # end
+    # Do not fallback to email as a way to find a user, we rely only on Identity record
     def create_or_find_user
       generated_password = SecureRandom.hex
 
-      if verified_email.blank?
-        @user = Decidim::User.new(
-          email: "",
-          organization: organization,
-          name: form.name,
-          nickname: form.normalized_nickname,
-          newsletter_notifications_at: nil,
-          email_on_notification: false,
-          password: generated_password,
-          password_confirmation: generated_password
-        )
-        @user.skip_confirmation!
-      else
-        @user = Decidim::User.find_or_initialize_by(
-          email: verified_email,
-          organization: organization
-        )
-
-        if @user.persisted?
-          # If user has left the account unconfirmed and later on decides to sign
-          # in with omniauth with an already verified account, the account needs
-          # to be marked confirmed.
-          @user.skip_confirmation! if !@user.confirmed? && @user.email == verified_email
-        else
-          @user.name = form.name
-          @user.email = form.email || verified_email
-          @user.nickname = form.normalized_nickname
-          @user.newsletter_notifications_at = nil
-          @user.email_on_notification = true
-          @user.password = generated_password
-          @user.password_confirmation = generated_password
-          @user.remote_avatar_url = form.avatar_url if form.avatar_url.present?
-          @user.skip_confirmation!
-        end
-      end
+      @user = Decidim::User.new(
+        email: "",
+        organization: organization,
+        name: form.name,
+        nickname: form.normalized_nickname,
+        newsletter_notifications_at: nil,
+        email_on_notification: false,
+        password: generated_password,
+        password_confirmation: generated_password
+      )
+      @user.skip_confirmation!
 
       @user.save!
     end
     # rubocop:enable Metrics/PerceivedComplexity
+
+    def existing_identity
+      return @existing_identity if @existing_identity.present?
+
+      @existing_identity ||= Identity.find_by(
+        user: organization.users,
+        provider: form.provider,
+        uid: form.uid
+      )
+
+      rrn = form.raw_data.dig(:info, :rrn)
+      if !@existing_identity && rrn.present?
+        enterprise_context_identity = Identity.find_by(
+          user: organization.users,
+          provider: 'saml',
+          rrn_hash: Digest::SHA256.base64digest(rrn)
+        )
+        if enterprise_context_identity.present? && enterprise_context_identity.user.persisted?
+          @user = enterprise_context_identity.user
+          @existing_identity = create_identity
+        end
+      end
+
+      @existing_identity
+    end
   end
 end
 
