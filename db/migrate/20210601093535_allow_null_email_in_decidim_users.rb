9ea2c2f23418fc8f9c8059a8e78cc28326bdf63e
sergei-krylov <sergei.krylov@yahoo.com>
2021-06-01 13:14:19 +0300

    Allow nulls in decidim_users.email column - fix migration class name

--- db/migrate/20210601093535_allow_null_email_in_decidim_users.rb
+++ db/migrate/20210601093535_allow_null_email_in_decidim_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddRrnHashToIdentities < ActiveRecord::Migration[5.2]
+class AllowNullEmailInDecidimUsers < ActiveRecord::Migration[5.2]
   def change
     change_column :decidim_users, :email, :string, null: true
   end
